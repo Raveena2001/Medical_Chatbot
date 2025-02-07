827635f2e3d67aa72463a62bbe3be5b3f8f50918
ravena <ravenaailapuram@gmail.com>
2025-02-07 17:49:40 +0530

    floder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative ai project",
+    version='0.0.0',
+    author= 'Raveena ',
+    author_email= "raveenaailapuram@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+
+)
